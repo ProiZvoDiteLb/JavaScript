8006583322de4caab92e72eda23ec82ee0e1ede8
Igor Ivanov <profrontend@mail.ru>
2025-04-01 18:49:51 +0300

    I created a variable with the release year of the first iphone and displayed it using the "alert()" method

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Задание 1 //
-
 let a = 10;
 alert(a);
 // console.log(a);
-
 a = 20;
 alert(a);
 // console.log(a);
+
+// Задание 2 //
+let releaseYear = 2007;
+alert(releaseYear);
+// console.log(releaseYear);
