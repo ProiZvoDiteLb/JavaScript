07ff28fd71b3879c710bbe08bbf64ad1a0ace1da
Igor Ivanov <profrontend@mail.ru>
2025-04-07 14:28:05 +0300

    создана ветка HomeWork3, создан отдельный файл hw3_password.js и подключен к отдельной странице index_JS.html, переход происходит при нажатии "ссылки" в футере "все права защищены"

---
+++ hw3_password.js
@@ -0,0 +1,20 @@
+// let password = prompt("Введите пароль");
+// switch (password) {
+//   case "123456":
+//     alert("пароль введен верно");
+//     // console.log("пароль введен верно");
+//     break;
+//   default:
+//     alert("пароль  НЕ верен");
+//   // console.log("пароль  НЕ верен");
+// }
+
+let password = prompt("Введите пароль");
+
+if (password === "123456") {
+  alert("пароль введен верно");
+  // console.log("пароль введен верно");
+} else {
+  alert("пароль  НЕ верен");
+  // console.log("пароль  НЕ верен");
+}
