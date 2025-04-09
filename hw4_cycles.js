dbe33da8e9a7b55e9822a1e3e88283489f6e45ce
Igor Ivanov <profrontend@mail.ru>
2025-04-09 20:53:21 +0300

    задание 3 ,вывел в консоль числа от 7 до 22 разными способами

--- hw4_cycles.js
+++ hw4_cycles.js
@@ -24,11 +24,37 @@
 //   i++;
 // } while (i <= 5);
 
-for (let i = 1; i <= 5; i++) {
-  console.log(i);
-}
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
 
 //__Задание 3__//
+
+// let i = 0;
+// while (i <= 22) {
+//   if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6) {
+//     i++;
+//     continue;
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 7;
+// while (i <= 22) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 7;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 22);
+
+// for (let i = 7; i <= 22; i++) {
+//   console.log(i);
+// }
 
 //__Задание 4__//
 
