e396cbcb83a1ecac1f1c1c838905682a337c9b6e
Igor Ivanov <profrontend@mail.ru>
2025-05-02 14:21:17 +0300

    Создана ветка HomeWork8, создан файл hw8_callback.js Проверить ДЗ

---
+++ hw8_callback.js
@@ -0,0 +1,37 @@
+// const fibonacci = [1, 1, 2, 3, 5, 8];
+// const multipleByTwoFib = fibonacci.map((num) => num * 2);
+// const plusTenFib = fibonacci.map((num) => num + 10);
+// const onlyBigFib = fibonacci.filter((num) => num > 3);
+
+// console.log(multipleByTwoFib); // [2, 2, 4, 6, 10, 16]
+// console.log(plusTenFib); // [11, 11, 12, 13, 15, 18]
+// console.log(onlyBigFib); // [5, 8]
+
+/////////////////////////////////
+///////////////////
+/////////////////////
+///////////////////////////////////////////////////////////
+// Создайте цикл, который выполняется от 0 до 9.
+
+// for (i = 0; i < 10; i++) {
+//   if (i == 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// function callbackWithArrayLength(arr, callback) {
+//   // console.log(arr);
+//   callback(arr.length);
+// }
+
+// callbackWithArrayLength([1], (length) => {
+//   console.log(length);
+// });
+
+// callbackWithArrayLength([1, 1], (len) => {
+//   console.log(len);
+// });
+// callbackWithArrayLength([1, 1, 1, 1, 1], (l) => {
+//   console.log(l);
+// });
